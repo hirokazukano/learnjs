e437bbc8b0d7478586714e699936415fa252b1d2
HirokazuKano <benzyamin2000@gmail.com>
2017-07-03 08:59:16 +0900

    Add first test

---
+++ public/app.js
@@ -0,0 +1,8 @@
+'use script';
+var learnjs = {};
+
+learnjs.showView = function (hash) {
+    var problemView = $('<div class="problem-view>"').text('Coming soon!');
+    $('.vew-container').empty().append(problemView);
+}
+
