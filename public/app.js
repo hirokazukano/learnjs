3573698fe03d6aafc5e6d832a61e2bdbb307aafa
HirokazuKano <benzyamin2000@gmail.com>
2017-07-04 11:42:53 +0900

    Update 2300 spy testを追加

--- public/app.js
+++ public/app.js
@@ -1,18 +1,19 @@
 'use script';
 var learnjs = {};
 
-learnjs.probleView = function () {
+learnjs.problemView = function () {
     return $('<div class="problem-view">').text('Coming soon!');
 }
 
 learnjs.showView = function (hash) {
     var routes = {
-        '#problem-1' : learnjs.probleView
+        '#problem' : learnjs.problemView
     };
 
-    var viewFn = routes[hash];
+    var hashParts = hash.split('-');
+    var viewFn = routes[hashParts[0]];
 
     if (viewFn) {
-        $('.view-container').empty().append(viewFn());
+        $('.view-container').empty().append(viewFn(hashParts[1]));
     }
 }
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -9,6 +9,11 @@ describe('LeanJS', function () {
         expect($('.view-container .landing-view').length).toEqual(1);
     })
 
+    it('passes the hash view parameter to the view function', function () {
+        spyOn(learnjs, 'problemView');
+        learnjs.showView('#problem-42');
+        expect(learnjs.problemView).toHaveBeenCalledWith('42');
+    })
 });
 
 
