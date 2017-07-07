007c3f05162c43a22ab884956f0f726ebeea1558
HirokazuKano <benzyamin2000@gmail.com>
2017-07-07 09:20:29 +0900

    Update 3200 add data-name selector

--- public/app.js
+++ public/app.js
@@ -4,17 +4,19 @@ var learnjs = {};
 learnjs.problems = [
   {
     description: "What is truth?",
-    dode: "function problem() { return __; }"
+    code: "function problem() { return __; }"
   },
   {
     description: "Simple Math",
-    dode: "function problem() { return 42 === 6 * __; }"
+    code: "function problem() { return 42 === 6 * __; }"
   }
 ];
 
-learnjs.problemView = function (problemNumber) {
+learnjs.problemView = function (data) {
+  var problemNumber = parseInt(data, 10);
   var view = $('.templates .problem-view').clone();
-  view.find('.title').text('Problem #' + problemNumber + ' Coming soon!');
+  view.find('.title').text('Problem #' + problemNumber);
+  learnjs.applyObject(learnjs.problems[problemNumber - 1], view);
   return view;
 }
 
@@ -29,11 +31,17 @@ learnjs.showView = function (hash) {
   if (viewFn) {
       $('.view-container').empty().append(viewFn(hashParts[1]));
   }
-}
+};
 
 learnjs.appOnReady = function () {
   window.onhashchange = function () {
     learnjs.showView(window.location.hash);
   };
   learnjs.showView(window.location.hash);
-}
+};
+
+learnjs.applyObject = function (obj, elem) {
+  for (var key in obj) {
+    elem.find('[data-name="' + key + '"]').text(obj[key]);
+  }
+};
