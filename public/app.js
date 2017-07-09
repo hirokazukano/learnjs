bbb73bc351e11f4eb136514380c82ad9fffcb69a
HirokazuKano <benzyamin2000@gmail.com>
2017-07-09 09:39:16 +0900

    Update 3500 正解、不正解表示にfadeIn()を使用

--- public/app.js
+++ public/app.js
@@ -32,9 +32,9 @@ learnjs.problemView = function (data) {
 
   function checkAnswerClick() {
     if (checkAnswer()) {
-      resultFlash.text('Correct!');
+      learnjs.flashElement(resultFlash, 'Correct!');
     } else {
-      resultFlash.text('Incorrect!');
+      learnjs.flashElement(resultFlash, 'Incorrect!');
     }
     return false;
   }
@@ -63,4 +63,11 @@ learnjs.appOnReady = function () {
     learnjs.showView(window.location.hash);
   };
   learnjs.showView(window.location.hash);
+};
+
+learnjs.flashElement = function (elem, content) {
+  elem.fadeOut('fast', function () {
+    elem.html(content);
+    elem.fadeIn()
+  });
 };
