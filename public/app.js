13f316fb5db536ee51808e5ad70420900589aef4
HirokazuKano <benzyamin2000@gmail.com>
2017-07-17 08:54:52 +0900

    Update 4300 4301 headerに登録アドレス表示を追加

--- public/app.js
+++ public/app.js
@@ -17,24 +17,30 @@ learnjs.problems = [
   }
 ];
 
-learnjs.triggerEvent = function (name, args) {
-  $('.view-container>*').trigger(name, args)
+learnjs.triggerEvent = function(name, args) {
+  $('.view-container>*').trigger(name, args);
 }
 
-learnjs.template = function (name) {
-  return $('.templates .' + name);
-};
+learnjs.template = function(name) {
+  return $('.templates .' + name).clone();
+}
 
-learnjs.applyObject = function (obj, elem) {
+learnjs.applyObject = function(obj, elem) {
   for (var key in obj) {
     elem.find('[data-name="' + key + '"]').text(obj[key]);
   }
+};
+
+learnjs.addProfileLink = function(profile) {
+  var link = learnjs.template('profile-link');
+  link.find('a').text(profile.email);
+  $('.signin-bar').prepend(link);
 }
 
-learnjs.flashElement = function (elem, content) {
-  elem.fadeOut('fast', function () {
+learnjs.flashElement = function(elem, content) {
+  elem.fadeOut('fast', function() {
     elem.html(content);
-    elem.fadeIn()
+    elem.fadeIn();
   });
 }
 
@@ -50,23 +56,22 @@ learnjs.buildCorrectFlash = function (problemNum) {
   return correctFlash;
 }
 
-learnjs.problemView = function (data) {
+learnjs.problemView = function(data) {
   var problemNumber = parseInt(data, 10);
-  var view = $('.templates .problem-view').clone();
+  var view = learnjs.template('problem-view');
   var problemData = learnjs.problems[problemNumber - 1];
   var resultFlash = view.find('.result');
 
   function checkAnswer() {
     var answer = view.find('.answer').val();
     var test = problemData.code.replace('__', answer) + '; problem();';
-    return eval(test)
+    return eval(test);
   }
 
   function checkAnswerClick() {
     if (checkAnswer()) {
-      var correctFlash = learnjs.template('correct-flash');
-      correctFlash.find('a').attr('href', '#problem-' + (problemNumber + 1));
-      learnjs.flashElement(resultFlash, correctFlash);
+      var flashContent = learnjs.buildCorrectFlash(problemNumber);
+      learnjs.flashElement(resultFlash, flashContent);
     } else {
       learnjs.flashElement(resultFlash, 'Incorrect!');
     }
@@ -88,49 +93,49 @@ learnjs.problemView = function (data) {
   return view;
 }
 
-learnjs.landingView = function () {
+learnjs.landingView = function() {
   return learnjs.template('landing-view');
 }
 
-learnjs.profileView = function () {
+learnjs.profileView = function() {
   var view = learnjs.template('profile-view');
-  learnjs.identity.done(function (identity) {
-    veiw.find('.email').text(identity.email);
+  learnjs.identity.done(function(identity) {
+    console.log(identity);
+    view.find('.email').text(identity.email);
   });
   return view;
 }
 
-learnjs.showView = function (hash) {
+learnjs.showView = function(hash) {
   var routes = {
-    '#problem' : learnjs.problemView,
-    '#profile' : learnjs.profileView,
+    '#problem': learnjs.problemView,
+    '#profile': learnjs.profileView,
     '#': learnjs.landingView,
     '': learnjs.landingView
   };
-
   var hashParts = hash.split('-');
   var viewFn = routes[hashParts[0]];
-
   if (viewFn) {
     learnjs.triggerEvent('removingView', []);
     $('.view-container').empty().append(viewFn(hashParts[1]));
   }
 }
 
-learnjs.appOnReady = function () {
-  window.onhashchange = function () {
+learnjs.appOnReady = function() {
+  window.onhashchange = function() {
     learnjs.showView(window.location.hash);
   };
   learnjs.showView(window.location.hash);
+  learnjs.identity.done(learnjs.addProfileLink);
 }
 
-learnjs.awsRefresh = function () {
+learnjs.awsRefresh = function() {
   var deferred = new $.Deferred();
-  AWS.config.credentials.refresh(function (err) {
+  AWS.config.credentials.refresh(function(err) {
     if (err) {
       deferred.reject(err);
     } else {
-      deferred.resolve(AWS.config.credentials.identityId)
+      deferred.resolve(AWS.config.credentials.identityId);
     }
   });
   return deferred.promise();
@@ -147,25 +152,21 @@ function googleSignIn(googleUser) {
       }
     })
   })
-
   function refresh() {
     return gapi.auth2.getAuthInstance().signIn({
       prompt: 'login'
-    }).then(function (userUpdate) {
+    }).then(function(userUpdate) {
       var creds = AWS.config.credentials;
       var newToken = userUpdate.getAuthResponse().id_token;
       creds.params.Logins['accounts.google.com'] = newToken;
       return learnjs.awsRefresh();
     });
   }
-  learnjs.awsRefresh().then(function (id) {
+  learnjs.awsRefresh().then(function(id) {
     learnjs.identity.resolve({
       id: id,
       email: googleUser.getBasicProfile().getEmail(),
       refresh: refresh
     });
   });
-  console.log(googleUser.getAuthResponse());
 }
-
-
